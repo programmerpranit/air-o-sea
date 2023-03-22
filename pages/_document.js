2a9d86f7bb49a1c6bf45f8f99f68f9c2c8a7212f
Pranit <dev.pranitpatil@gamail.com>
2023-03-22 20:41:11 +0530

    tailwind setup

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Air O Sea</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
