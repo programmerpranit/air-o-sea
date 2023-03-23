a387573190ea32a087d9cae9489cbda59f713e69
Pranit <pranit@webxstudio.in>
2023-03-23 21:59:00 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Center from "@/components/Center";
 import Footer from "@/components/Footer";
 import Homepage from "@/components/Homepage";
 import Navbar from "@/components/Navbar";
-import { Main } from "next/document";
 
 export default function Home() {
   return (
