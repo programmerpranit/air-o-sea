7baf805867bcdba887a6487f4bec2be4e160d900
Pranit <pranit@webxstudio.in>
2023-03-28 16:15:16 +0530

    done

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <div className="p-4 pt-10 bg-primary bg-opacity-5 mt-20 border-t-2">
-      <div className="">
+    <div className="p-4 pt-10 bg-primary  bg-opacity-5 mt-20 border-t-2">
+      <div className="flex max-md:flex-col gap-6 max-w-4xl m-auto">
+
+      <div className="md:w-1/2 md:p-10">
         <h5 className="text-bold text-lg my-3 text-primary">About Us</h5>
         <p>
           We are full service international organization having a registered
@@ -10,8 +12,7 @@ export default function Footer() {
           entrepreneurs, high scale companies, energy generation organizations.
         </p>
       </div>
-      <br/>
-      <div className="my-5">
+      <div className="my-5 md:p-10">
         <h5 className="text-bold text-lg my-3 text-primary">Official Info</h5>
         <p>Registered Office: Hong Kong</p>
         <p>Corporate Office: Guangzhou (China)</p>
@@ -20,12 +21,12 @@ export default function Footer() {
         <p>Mon – Sat: 8 am – 5 pm</p>
         <p>Sunday: CLOSED</p>
       </div>
+    </div>
       <div className="flex flex-col my-4 text-footer_text">
         <p className="text-center my-2">
           2023 © All rights reserved by Air – O – Sea International Pvt Ltd.
         </p>
-        
       </div>
-    </div>
+      </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import AboutUs from "@/sections/AboutUs";
 import Hero from "@/sections/Hero";
 import Services from "@/sections/Services";
 import WhyChooseUs from "@/sections/WhyChooseUs";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,6 +15,14 @@ export default function Home() {
       <Services/>
       <WhyChooseUs />
 
+      <div className="fixed z-50 bottom-5 right-5">
+        <a href="">
+          <Image className="m-auto mb-3 bg-white border rounded-full p-2" src={'/call.png'} width={40} height={40} />
+        </a>
+        <a href="">
+          <Image src={'/whatsapp.png'} width={50} height={50} />
+        </a>
+      </div>
     </>
   );
 }
